0277e4526ae7baf4ea5693514e8157952a30368b
CodaOfHonor <tnaveen8015@gmail.com>
2017-04-18 22:21:20 -0500

    updated cachematrix.R file with code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As the assignment required, created two functions, makeCacheMatrix and cacheSolve
+## makeCacheMatrix defines the four functions that will be returned as an argument
+## cacheSolve checks for if a repeat matrix is being used(whereby it returns existing value in the cache)
+## Process to find inverse of a matrix: 
+## 1. assign matrix; 2. run makeCacheMatrix on matrixl; and 3. use the result of 2 as an argument in cacheSolve
+
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
